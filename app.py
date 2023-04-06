413d418b95a6695f37fddbf9fe60d2757af314ce
Alisher250 <81162609+Alisher250@users.noreply.github.com>
2023-04-06 23:54:34 +0600

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def causes():
     x=[]
     x1 = ''
     x2 = ''
-    openai.api_key = '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'
+    openai.api_key = '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'
     if request.method == "POST":
         file = form.file.data
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
