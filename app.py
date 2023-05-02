4ab7ab89e1522398b64184a361e979ee62833130
Alisher250 <81162609+Alisher250@users.noreply.github.com>
2023-05-02 22:18:22 +0600

    Update app.py

--- app.py
+++ app.py
@@ -49,31 +49,13 @@ def causes():
     x=[]
     x1 = ''
     x2 = ''
-    openai.api_key = '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'
     if request.method == "POST":
         file = form.file.data
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
         x = execute()
         x1 = 'On the pictures you can see:' \
              '1 - ' + x[0][0], '2 - ' + x[0][1], '3 - ' + x[0][2], '4 - ' + x[0][3], '5 - ' + x[0][4]
-
-
-        prompt = f'Act like a Dermamarker, which is ai that detects and explains, recommends how to treat skin disease. Firstly, Interpret what is the possibility of skin diseases of the person(client) according to list below. Then recommend what to do. Dont sфн that it is a provided list. Say like you did this analysis. Be confident, and say everything like you did it all. Exactly say statistics to make sure client understands. Dont recommend dermatologist. Explain what is it and what to do in 500 words minimum. Write in a structred, specific way [{x[3]}]'
-        # completion = openai.ChatCompletion.create(
-        #     model = 'gpt-3.5-turbo',
-        #     messages = [
-        #         {'role': 'user', 'content': prompt}
-        #     ]
-        # )
-        # x2 = completion.choices[0].message.content
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=1024,
-            stop=None,
-            temperature=0.5,
-        )
-        x2 = response.choices[0].text.strip()
+        x2 = "Fuck you"
 
     return render_template('causes.html', form=form, x=x, x1=x1, x2=x2)
 # https://www.google.kz/search?q=2DnHWraKtR4v5vUzBbSy0FHEN23_5KfXy9zkA83dL9UpPgzFA&ie=UTF-8&oe=UTF-8&hl=en-kz&client=safari
